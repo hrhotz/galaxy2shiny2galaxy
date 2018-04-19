7bd805170bdd2341bfb2b4a5cf7a1705e32017cf
Hans-Rudolf Hotz <hrhotz@users.noreply.github.com>
2018-04-19 13:19:45 +0200

    Create dataset2history_id.wrapper.sh

---
+++ helper_scripts/dataset2history_id.wrapper.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# 
+
+# version 1.0.0 
+
+
+###REPLACE### export PYTHONPATH=/PATH/TO/pg module
+
+###REPLACE### LD_LIBRARY_PATH=/PATH/TO/PostgreSQL/pgsql-version/lib/
+
+###REPLACE### /PATH/TO/api_helpers/dataset2history_id.py $1
+
+
