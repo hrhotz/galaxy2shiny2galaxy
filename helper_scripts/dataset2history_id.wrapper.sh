d1565c680f4f95d4a0f8f4979e4db87d3f357750
Hans-Rudolf Hotz <hrhotz@users.noreply.github.com>
2018-04-20 13:53:39 +0200

    Update dataset2history_id.wrapper.sh

--- helper_scripts/dataset2history_id.wrapper.sh
+++ helper_scripts/dataset2history_id.wrapper.sh
@@ -8,6 +8,6 @@
 
 ###REPLACE### LD_LIBRARY_PATH=/PATH/TO/PostgreSQL/pgsql-version/lib/
 
-###REPLACE### /PATH/TO/api_helpers/dataset2history_id.py $1
+###REPLACE### GALAXYROOT/tools/galaxy2shiny2galaxy/helper_scripts/dataset2history_id.py $1
 
 
